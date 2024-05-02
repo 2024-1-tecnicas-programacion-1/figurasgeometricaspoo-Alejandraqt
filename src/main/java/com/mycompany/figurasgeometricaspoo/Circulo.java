4e9e7609cd2e172ebc6a8ca797c247e261a5b6aa
Juan <example@gmail.com>
2024-05-02 10:34:37 -0500

    Actualización métodos

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,42 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Circulo {
+public class Circulo extends FiguraGeometrica {
     
+    private double radio;
+
+     /**
+     * Constructor de la clase Pantalon.
+     *
+     * @param nombre
+     * @param color
+     * @param radio
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    /**
+     * Método para 
+     * Redefine el método obtenerArea de la clase padre FiguraGeometrica.
+     * 
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    @Override
+    public double obtenerArea() {
+        return super.obtenerArea();
+    }
+    
+    /**
+     * Método para 
+     * Redefine el método obtenerPerimetro de la clase padre FiguraGeometrica.
+     * 
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    @Override
+    public double obtenerPerimetro() {
+        return super.obtenerPerimetro();
+    }
 }
--- src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
@@ -1,5 +1,44 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    /**
+     * Constructor de la clase.
+     * 
+     * @param nombre nombre de la figura geometrica
+     * @param color color de la figura
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    /**
+     * Método para 
+     * 
+     * @return 
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public double obtenerArea() {
+        //
+        return 0.0;
+    }
+
+    /**
+     * Método para 
+     * 
+     * @return 
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public double obtenerPerimetro() {
+        //
+        return 0.0;
+    }
     
 }
